0b9cfa7eb986b84d2804fc4ab2dea14c956393cb
Akash Shinde <saakashs1999@gmail.com>
2024-11-20 17:56:57 +0530

    1

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /home/ubuntu/code
-pm2 stop mqtt
+
+if pm2 list | grep -q "mqtt"; then
+    echo "stopping the application........."
+    pm2 stop mqtt
+    pm2 delete mqtt
+else
+    echo "Application is not running. Skipping the step"
+fi
+
+# pm2 stop mqtt
 # npm install 
